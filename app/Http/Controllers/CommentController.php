8b08552e834379e342c396a21f2e9dba35575cab
yanshuxin <359569219@qq.com>
2017-10-16 16:22:06 +0800

    update addComment 20

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -50,18 +50,18 @@
 //		    		}
 		 
 		    		$filepath = $this->file_path.$image_name;
-		    		return $this->output(Response::SUCCESS, $filepath);
 		    		
-//		    		if ( file_put_contents($filepath, base64_decode(str_replace($result[1], '', $imageAry[$i]))) )
-//		    		{
-//		    			$id = DB::table('sc_image')->insertGetId([
-//		    				'filepath' => date('Ymd',time()).$image_name
-//		    			]);
-//		    		}
-//		    		else
-//		    		{
-//		    			
-//		    		}
+		    		if ( file_put_contents($filepath, base64_decode(str_replace($result[1], '', $imageAry[$i]))) )
+		    		{
+		    			$id = DB::table('sc_image')->insertGetId([
+		    				'filepath' => date('Ymd',time()).$image_name
+		    			]);
+		    			return $this->output(Response::SUCCESS, $id);
+		    		}
+		    		else
+		    		{
+		    			
+		    		}
 	    		}
 	    		else
 	    		{

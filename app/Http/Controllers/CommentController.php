c259396bc86e1ad36a1d7be47cad7c4604d568a7
yanshuxin <359569219@qq.com>
2017-10-17 13:04:54 +0800

    update addComment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -21,58 +21,34 @@
 	    
 	    public function addComment(Request $request)
 	    {
-//	    	$uid = $request->input('uid');
-//	    	$star = $request->input('star');
-//	    	$taste = $request->input('taste');
-//	    	$enviroment = $request->input('enviroment');
-//	    	$serve = $request->input('serve');
-//	    	$content = $request->input('content');
-//	    	$type = $request->input('type');
+	    	$uid = $request->input('uid');
+	    	$star = $request->input('star');
+	    	$taste = $request->input('taste');
+	    	$enviroment = $request->input('enviroment');
+	    	$serve = $request->input('serve');
+	    	$content = $request->input('content');
+	    	$type = $request->input('type');
+	    	$parentid = $request->input('parentid');
 	    	$imageAry = $request->input('imageAry');
-	    	
-	    	for ( $i = 0 ; $i < count($imageAry) ; $i++ )
+	    	$targetid = $request->input('targetid');
+	    	$commentid = DB::table('sc_commit')->insertGetId([
+	    		'uid' => $uid,
+	    		'type' => $type,
+	    		'time' => time(),
+	    		'content' => $content,
+	    		'parentid' => $parentid,
+	    		'taste' => $taste,
+	    		'environment' => $enviroment,
+	    		'service' => $serve,
+	    		'targetid' => $targetid
+	    	]);
+	    	if ( !empty($commentid) )
 	    	{
-	    		if ( preg_match('/^(data:\s*image\/(\w+);base64,)/', $imageAry[$i], $result) )
-	    		{
-	    			//匹配成功 
-		    		if ( $result[2] == 'jpeg' )
-		    		{
-		    			$image_name = date('YmdHis').time().'.jpg';
-		    		}
-		    		else
-		    		{
-		    			$image_name = date('YmdHis').time().'.'.$result[2];
-		    		}
-		    		
-		    		return $this->output(Response::SUCCESS, file_exists($this->file_path.date('Ymd',time()).'/'));
-//		    		if ( !file_exists($this->file_path.date('Ymd',time()).'/') )
-//		    		{
-//		    			return $this->output(Response::SUCCESS, $this->file_path.date('Ymd',time()).'/');
-//	    				mkdir($this->file_path.date('Ymd',time()).'/', 0700);
-//		    		}
-//		    		else
-//		    		{
-//		    			return $this->output(Response::SUCCESS, "yanshuxin");
-//		    		}
-		 
-//		    		$filepath = $this->file_path.$image_name;
-//		    		
-//		    		if ( file_put_contents($filepath, base64_decode(str_replace($result[1], '', $imageAry[$i]))) )
-//		    		{
-//		    			$id = DB::table('sc_image')->insertGetId([
-//		    				'filepath' => $image_name
-//		    			]);
-//		    			return $this->output(Response::SUCCESS, $id);
-//		    		}
-//		    		else
-//		    		{
-//		    			
-//		    		}
-	    		}
-	    		else
-	    		{
-	    			
-	    		}
+	    		return $this->output(Response::SUCCESS);
+	    	}
+	    	else
+	    	{
+	    		return $this->output(Response::WRONG_OPERATION);
 	    	}
 	    }
 	    
